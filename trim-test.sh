4af88206c9d50262c23f28443fc32a6b824eee48
Jason W. Bacon <bacon4000@gmail.com>
2023-06-17 13:47:05 -0500

    show-adapters: Run fastq-scum to show adapters, poly-As, etc Frame out some early analysis scripts

--- trim-test.sh
+++ trim-test.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -e
 
-#fastq-scum Raw/A16Na1/A16Na1_1.fq.gz 
-#fastq-scum Raw/A16Na1/A16Na1_2.fq.gz
-
 export GZIP=-5
-fastq-trim \
+time fastq-trim \
     --3p-adapter1 AGATCGGAAGAG \
     --3p-adapter2 AGATCGGAAGAG \
     Raw/A16Na1/A16Na1_1.fq.gz out_1.fq.gz \
