b4092b4bd8e0a4b705adaf2b25a3968b075ee5bb
Jason W. Bacon <bacon4000@gmail.com>
2023-06-21 11:17:17 -0500

    Add kallisto index scripts and rerun.sh

--- 07-reference.sh
+++ 07-reference.sh
@@ -3,9 +3,19 @@
 # From Ava email: https://www.axolotl-omics.org/
 # "file" command revealed name within gzip data to use with --output
 # Firefox "save link as" comes up with .zip, which is wrong.  Should be .gz.
-cd Results/07-reference
-curl --continue-at - --location --output AmexT_v47_cds.fa.gz \
-    'http://www.axolotl-omics.org/api?method=Assembly.getSequences&assembly=47&type=cds'
-curl --continue-at - --location --output AmexT_v47_dna.fa.gz \
-    'http://www.axolotl-omics.org/api?method=Assembly.getSequences&assembly=47&type=dna'
 
+cd Results/07-reference
+
+if [ ! -e AmexT_v47_cds.fa ]; then
+    curl --continue-at - --location --output AmexT_v47_cds.fa.gz \
+	'http://www.axolotl-omics.org/api?method=Assembly.getSequences&assembly=47&type=cds'
+    printf "Deflating...\n"
+    gunzip AmexT_v47_cds.fa.gz
+fi
+
+if [ ! -e AmexT_v47_dna.fa ]; then
+    curl --continue-at - --location --output AmexT_v47_dna.fa.gz \
+	'http://www.axolotl-omics.org/api?method=Assembly.getSequences&assembly=47&type=dna'
+    printf "Deflating...\n"
+    gunzip AmexT_v47_dna.fa.gz
+fi
