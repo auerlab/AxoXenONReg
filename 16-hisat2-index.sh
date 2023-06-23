f7c983d04e53d1335f3da1f3aa424976498c7e9f
Jason W. Bacon <bacon4000@gmail.com>
2023-06-23 08:47:56 -0500

    Work on 17-hisat2-align gz2zst.sbatch: Create zstd equivalents for trimmed fastas

---
+++ 16-hisat2-index.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+
+##########################################################################
+#   Script description:
+#       Run quality checks on raw and trimmed data for comparison
+#       Based on work of Dr. Andrea Rau:
+#       https://github.com/andreamrau/OpticRegen_2019
+#
+#   Dependencies:
+#       Requires directory structure.  Run after *-organize.sh.
+#
+#       All necessary tools are assumed to be in PATH.  If this is not
+#       the case, add whatever code is needed here to gain access.
+#       (Adding such code to your .bashrc or other startup script is
+#       generally a bad idea since it's too complicated to support
+#       every program with one environment.)
+#
+#   History:
+#   Date        Name        Modification
+#   2023-06-02  Jason Bacon Begin
+##########################################################################
+
+if which sbatch; then
+    sbatch SLURM/16-hisat2-index.sbatch
+else
+    # Debug
+    # rm -f Results/16-hisat2-index/*
+    
+    hw_threads=$(./get-hw-threads.sh)
+    jobs=$(($hw_threads / 2))
+    # Tried GNU parallel and ran into bugs.  Xargs just works.
+    ls Results/01-organize/Raw-renamed/*-R1.fastq.xz | \
+	xargs -n 1 -P $jobs Xargs/16-hisat2-index.sh
+fi
---
+++ Reference/genome-filename.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -e
+
+printf "AmexG_v6.0-DD.fa\n"
