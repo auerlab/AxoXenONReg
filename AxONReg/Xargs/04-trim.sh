b17d74a5e6a8f68fc4d981f50420a72c9cd602e0
Jason Bacon <bacon@NetBSD.org>
2023-07-16 19:56:44 -0500

    Xargs/04-trim: gzip -> zstd

--- AxONReg/Xargs/04-trim.sh
+++ AxONReg/Xargs/04-trim.sh
@@ -3,10 +3,10 @@
 raw1=$1
 base=$(basename $raw1)
 stem=${base%%-R1.fastq.xz}
-trimmed1=Results/04-trim/$stem-R1.fastq.gz
+trimmed1=Results/04-trim/$stem-R1.fastq.zst
 
 raw2=${raw1%%-R1.fastq.xz}-R2.fastq.xz
-trimmed2=Results/04-trim/$stem-R2.fast1.gz
+trimmed2=Results/04-trim/$stem-R2.fast1.zst
 
 if [ -e $trimmed1 ]; then
     printf "$raw1 + $raw2 already processed.\n"
@@ -29,7 +29,7 @@ else
     # fastq-trim is 2.5x faster with 1 core than cutadapt with 2 cores
     # Our reads use the default Illumina universal adapter,
     # but we'll state it explicitly anyway
-    export GZIP=-1
+    # export GZIP=-1
     set -x
     time fastq-trim --3p-adapter1 $adapter --3p-adapter2 $adapter \
 	--min-qual 24 --polya-min-length 4 $raw1 $trimmed1 $raw2 $trimmed2 \
