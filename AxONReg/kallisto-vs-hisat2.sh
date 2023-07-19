d65f15fb4dbd86918be8d02d07b452ec648bbcd8
Jason W. Bacon <bacon4000@gmail.com>
2023-07-19 15:03:47 -0500

    Compute both exact and stringtie abundances for comparison

--- AxONReg/kallisto-vs-hisat2.sh
+++ AxONReg/kallisto-vs-hisat2.sh
@@ -15,11 +15,13 @@ pause()
 }
 
 # Abundances
-# kallisto=Results/10-fasda-kallisto/time1-all-norm.tsv
-# hisat2=Results/13-fasda-hisat/time1-all-norm.tsv
 kallisto=Results/09-kallisto-quant/sample01-time1-rep1/abundance.tsv
+hisat2_exact=Results/13-fasda-abundance-hisat2/sample01-time1-rep1-abundance-exact.tsv
 hisat2=Results/13-fasda-abundance-hisat2/sample01-time1-rep1-abundance.tsv
 # more $hisat2
+
+wc -l $kallisto $hisat2_exact $hisat2
+pause
 
 # FIXME: kallisto transcript IDs contain a ';'
 for transcript in $(awk '$1 != "target_id" { print $1 }' $hisat2); do
@@ -30,13 +32,39 @@ for transcript in $(awk '$1 != "target_id" { print $1 }' $hisat2); do
 	head -1 $hisat2
 	printf "Kallisto: "
 	fgrep $transcript $kallisto || true
-	printf "Hisat2:   "
+	printf "Hisat2e:  "
+	fgrep $transcript $hisat2_exact || true
+	printf "Hisat2s:  "
 	fgrep $transcript $hisat2 || true
 	
 	# Based on normalized counts
 	#k=$(awk -v t=$transcript '$1 == t { print $2 + $3 + $4 }' $kallisto)
 	#h=$(awk -v t=$transcript '$1 == t { print $2 + $3 + $4 }' $hisat2)
 	#printf "$h / ($k + .0000001)\n" | bc -l
+    fi
+done | more
+exit
+
+# Normalized
+kallisto=Results/10-fasda-kallisto/time1-all-norm.tsv
+hisat2=Results/13-fasda-hisat/time1-all-norm.tsv
+for transcript in $(awk '$1 != "target_id" { print $1 }' $hisat2); do
+    # printf "transcript = $transcript\n"
+    if fgrep -q $transcript $kallisto; then
+	printf '===\n'
+	printf "          "
+	head -1 $hisat2
+	printf "Kallisto: "
+	fgrep $transcript $kallisto || true
+	printf "Hisat2e:  "
+	fgrep $transcript $hisat2_exact || true
+	printf "Hisat2s:  "
+	fgrep $transcript $hisat2 || true
+	
+	# Based on normalized counts
+	k=$(awk -v t=$transcript '$1 == t { print $2 + $3 + $4 }' $kallisto)
+	h=$(awk -v t=$transcript '$1 == t { print $2 + $3 + $4 }' $hisat2)
+	printf "$h / ($k + .0000001)\n" | bc -l
     fi
 done | more
 
@@ -49,4 +77,4 @@ for transcript in $(awk '{ print $1 }' $hisat2 | head -100); do
 	grep $transcript $kallisto
 	grep $transcript $hisat2
     fi
-done
+done | more
