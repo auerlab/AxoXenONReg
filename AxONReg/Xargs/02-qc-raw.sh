1e48d1ef109057d6a3a174408f6f8b572853eb2e
Jason Bacon <bacon@NetBSD.org>
2023-07-23 09:11:12 -0500

    Clean up qc scripts

--- AxONReg/Xargs/02-qc-raw.sh
+++ AxONReg/Xargs/02-qc-raw.sh
@@ -1,23 +1,23 @@
 #!/bin/sh -e
 
 # Raw files from sequencing center
-raw=$1
+fastq=$1
 
 # Filename stems for fastqc output
-stem_raw=$(basename ${raw%.fastq.xz})
-results=Results/02-qc-raw/$(basename ${stem_raw})_fastqc.html
+stem_fastq=$(basename ${fastq%.fastq.xz})
+results=Results/02-qc-raw/$(basename ${stem_fastq})_fastqc.html
 
 if [ -e $results ]; then
-    printf "$raw already processed.\n"
+    printf "$fastq already processed.\n"
 else
-    printf "Processing $raw with fastqc...\n"
+    printf "Processing $fastq with fastqc...\n"
     
-    log_stem=Logs/02-qc-raw/$stem_raw
+    log_stem=Logs/02-qc-raw/$stem_fastq
     # Document software versions used for publication
     uname -a > $log_stem.out
     fastqc --version >> $log_stem.out
     pwd >> $log_stem.out
 
-    xzcat $raw | fastqc -o Results/02-qc-raw stdin:$stem_raw \
+    xzcat $fastq | fastqc -o Results/02-qc-raw stdin:$stem_fastq \
 	>> $log_stem.out 2>> $log_stem.err
 fi
--- AxONReg/Xargs/05-qc-trimmed.sh
+++ AxONReg/Xargs/05-qc-trimmed.sh
@@ -1,23 +1,23 @@
 #!/bin/sh -e
 
 # Trimmed files
-trimmed=$1
+fastq=$1
 
 # Filename stems for fastqc output
-stem_trimmed=$(basename ${trimmed%.fastq.xz})
-results=Results/05-qc-trimmed/$(basename ${stem_trimmed})_fastqc.html
+stem_fastq=$(basename ${fastq%.fastq.xz})
+results=Results/05-qc-trimmed/$(basename ${stem_fastq})_fastqc.html
 
 if [ -e $results ]; then
-    printf "$trimmed already processed.\n"
+    printf "$fastq already processed.\n"
 else
-    printf "Processing $trimmed with fastqc...\n"
+    printf "Processing $fastq with fastqc...\n"
     
-    log_stem=Logs/05-qc-trimmed/$stem_trimmed
+    log_stem=Logs/05-qc-trimmed/$stem_fastq
     # Document software versions used for publication
     uname -a > $log_stem.out
     fastqc --version >> $log_stem.out
     pwd >> $log_stem.out
 
-    zstdcat $trimmed | fastqc -o Results/05-qc-trimmed stdin:$stem_trimmed \
+    zstdcat $fastq | fastqc -o Results/05-qc-trimmed stdin:$stem_fastq \
 	>> $log_stem.out 2>> $log_stem.err
 fi
