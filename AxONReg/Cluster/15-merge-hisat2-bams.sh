4f9953b6689bda10bab323ca1ff590242a8cc712
Jason W. Bacon <bacon4000@gmail.com>
2023-07-29 17:14:53 -0500

    18-merge-hisat2-bams -> 15-merge-hisat2-bams

--- AxONReg/Cluster/15-merge-hisat2-bams.sh
+++ AxONReg/Cluster/15-merge-hisat2-bams.sh
@@ -20,12 +20,4 @@
 #   2023-06     Jason Bacon Begin
 ##########################################################################
 
-if which sbatch; then
-    sbatch SLURM/18-merge-hisat2-bams.sbatch
-else
-    hw_threads=$(../Common/get-hw-threads.sh)
-    jobs=$(($hw_threads / 2))
-    # Tried GNU parallel and ran into bugs.  Xargs just works.
-    ls Results/01-organize/Raw-renamed/*-R1.fastq.xz | \
-	xargs -n 1 -P $jobs Xargs/18-merge-hisat2-bams.sh
-fi
+sbatch SLURM/15-merge-hisat2-bams.sbatch
--- AxONReg/No-cluster/15-merge-hisat2-bams.sh
+++ AxONReg/No-cluster/15-merge-hisat2-bams.sh
@@ -20,12 +20,9 @@
 #   2023-06     Jason Bacon Begin
 ##########################################################################
 
-if which sbatch; then
-    sbatch SLURM/18-merge-hisat2-bams.sbatch
-else
-    hw_threads=$(../Common/get-hw-threads.sh)
-    jobs=$(($hw_threads / 2))
-    # Tried GNU parallel and ran into bugs.  Xargs just works.
-    ls Results/01-organize/Raw-renamed/*-R1.fastq.xz | \
-	xargs -n 1 -P $jobs Sh/18-merge-hisat2-bams.sh
-fi
+hw_threads=$(../Common/get-hw-threads.sh)
+jobs=$(($hw_threads / 2))
+# Tried GNU parallel and ran into bugs.  Xargs just works.
+ls Results/01-organize/Raw-renamed/*-R1.fastq.xz | \
+    xargs -n 1 -P $jobs Sh/15-merge-hisat2-bams.sh
+
