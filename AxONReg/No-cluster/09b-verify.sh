68f9ece752328b9b505529f712a5ebafc3d22a43
Jason Bacon <bacon@NetBSD.org>
2023-07-30 09:03:54 -0500

    09b-verify: Show head for each abundance file

--- AxONReg/No-cluster/09b-verify.sh
+++ AxONReg/No-cluster/09b-verify.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -e
 
+
+##########################################################################
+#   Function description:
+#       Pause until user presses return
+##########################################################################
+
+pause()
+{
+    local junk
+    
+    printf "Press return to continue..."
+    read junk
+}
+
 more Logs/09-kallisto-quant/*.out
 more Logs/09-kallisto-quant/*.err
-more Results/09-kallisto-quant/*/abundance.tsv
+head Results/09-kallisto-quant/*/abundance.tsv | more
